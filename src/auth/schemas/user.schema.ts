c2ead1c3616b92e614a9c74531910ca4b87c25a0
Taiwo Adejare <adejareemma@gmail.com>
2021-12-17 16:23:20 +0100

    mongoose schema for users

---
+++ src/auth/schemas/user.schema.ts
@@ -0,0 +1,21 @@
+import * as mongoose from 'mongoose';
+
+export const UserSchema = new mongoose.Schema({
+  firstname: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
