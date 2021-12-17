7035d21a00c3714d5b7890b4f531a964715c1e36
Taiwo Adejare <adejareemma@gmail.com>
2021-12-17 16:20:50 +0100

    auth module config to use jwt

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { UserSchema } from './schemas/user.schema';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: {
+        expiresIn: '1S',
+      },
+    }),
+    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService],
+})
+export class AuthModule {}
