ba2504474dbad66f2bf1e59c9983d961a9523dab
Taiwo Adejare <adejareemma@gmail.com>
2021-12-18 01:35:10 +0100

    chore: controller for post CRUD

---
+++ src/post/post.controller.ts
@@ -0,0 +1,41 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Put,
+  ValidationPipe,
+} from '@nestjs/common';
+import { SuccessResponse } from 'src/interfaces/response.interface';
+import { PostDto } from './dto/post.dto';
+import { PostService } from './post.service';
+
+@Controller('post')
+export class PostController {
+  constructor(private readonly postService: PostService) {}
+  @Get()
+  async getPosts(): Promise<SuccessResponse> {
+    return await this.postService.getAll();
+  }
+
+  @Get(':postId')
+  async getPost(@Param('postId') postId: string): Promise<SuccessResponse> {
+    return await this.postService.getById(postId);
+  }
+
+  @Post()
+  async createPost(
+    @Body(ValidationPipe) postDto: PostDto,
+  ): Promise<SuccessResponse> {
+    return await this.postService.create(postDto);
+  }
+
+  @Put(':postId')
+  async updatePost(
+    @Body(ValidationPipe) postDto: PostDto,
+    @Param('postId') postId: string,
+  ): Promise<SuccessResponse> {
+    return await this.postService.update(postId, postDto);
+  }
+}
